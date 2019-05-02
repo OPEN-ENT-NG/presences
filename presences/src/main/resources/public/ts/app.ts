9b3d6a3393c9a070735c5629bf7c1893dd69b756
Simon Ledunois <simon.ledunois@cgi.com>
2019-05-02 11:58:38 +0200

    Fix app.ts. Multiple directives declarations Add Unix cast on build.sh

--- presences/src/main/resources/public/ts/app.ts
+++ presences/src/main/resources/public/ts/app.ts
@@ -3,10 +3,6 @@ import {rights} from './rights'
 import * as controllers from './controllers';
 import * as directives from './directives';
 
-for (let directive in directives) {
-    ng.directives.push(directives[directive]);
-}
-
 for (let controller in controllers) {
     ng.controllers.push(controllers[controller]);
 }
