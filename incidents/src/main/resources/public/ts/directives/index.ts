afa05155cf814b8e7bdbdc074b5cc00e637ef472
Simon LEDUNOIS <simon.ledunois@gmail.com>
2019-08-23 16:52:46 +0200

    fix(incident): Remove double timeformat directive injection

--- incidents/src/main/resources/public/ts/directives/index.ts
+++ incidents/src/main/resources/public/ts/directives/index.ts
@@ -3,5 +3,4 @@ export * from '@common/directives/async-autocomplete';
 export * from '@common/directives/pagination';
 export * from '@common/directives/loader';
 export * from '@common/directives/timepicker';
-export * from '@common/directives/timeformat';
 export * from '@common/directives/toasts';
