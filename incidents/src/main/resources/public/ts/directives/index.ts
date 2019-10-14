37de96272b5a1ff60235bc353c4d6acab7632015
jinhda douang <s>
2019-10-14 15:45:25 +0200

    fix(parameter): #MA-378 Fix incidents screen param viesco

--- incidents/src/main/resources/public/ts/directives/index.ts
+++ incidents/src/main/resources/public/ts/directives/index.ts
@@ -2,5 +2,6 @@ export * from '@common/directives/staticInclude';
 export * from '@common/directives/async-autocomplete';
 export * from '@common/directives/pagination';
 export * from '@common/directives/loader';
+export * from '@common/directives/select-disabled';
 export * from '@common/directives/timepicker';
 export * from '@common/directives/toasts';
