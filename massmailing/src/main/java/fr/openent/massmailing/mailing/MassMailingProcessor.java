72957726d418cc2bf2c9db8737c987c57ae9d327
Simon LEDUNOIS <simon.ledunois@gmail.com>
2019-12-04 10:18:37 +0100

    fix(massmailing) #MA-407 fix event saving process

--- massmailing/src/main/java/fr/openent/massmailing/mailing/MassMailingProcessor.java
+++ massmailing/src/main/java/fr/openent/massmailing/mailing/MassMailingProcessor.java
@@ -381,7 +381,11 @@ public abstract class MassMailingProcessor implements Mailing {
             JsonArray list = events.getJsonArray(key);
             for (int i = 0; i < list.size(); i++) {
                 JsonObject item = list.getJsonObject(i);
-                evts.add(new JsonObject().put("id", item.getInteger("id")).put("type", key));
+                JsonArray embedEvts = item.getJsonArray("events");
+                for (int j = 0; j < embedEvts.size(); j++) {
+                    JsonObject evt = embedEvts.getJsonObject(j);
+                    evts.add(new JsonObject().put("id", evt.getInteger("id")).put("type", key));
+                }
             }
         }
 
