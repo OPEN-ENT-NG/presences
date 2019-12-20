d60c612e35c6b6a0523c3fa29128df5b563f1ac7
CaroleHermouet <carole.hermouet@cgi.com>
2019-12-20 16:46:09 +0100

    feature(presences): MA-423 Manage Alert (Put Threshold)

--- presences/src/main/resources/public/ts/sniplets/presences-manage/presences-alert-manage.ts
+++ presences/src/main/resources/public/ts/sniplets/presences-manage/presences-alert-manage.ts
@@ -1,3 +1,63 @@
+import {Setting, settingService} from '../../services/SettingsService';
+import {toasts} from "entcore";
+
+declare const window;
+
+interface ViewModel {
+    setting: Setting;
+
+    // interaction
+    getThreshold(): void;
+
+    incrementThreshold(): void;
+
+    decrementThreshold(): void;
+
+    save(): void;
+}
+
+const vm: ViewModel = {
+    setting: {},
+
+    async getThreshold() {
+        try {
+            let defaultSettings = {
+                alert_absence_threshold: 0,
+                alert_lateness_threshold : 0,
+                alert_incident_threshold : 0,
+                alert_forgotten_notebook_threshold : 0
+            };
+            let structureSettings: any = await settingService.retrieve(window.model.vieScolaire.structure.id);
+
+            vm.setting = {...defaultSettings, ...structureSettings};
+
+            presencesAlertManage.that.$apply();
+        } catch (e) {
+            toasts.warning('error');
+            throw e;
+        }
+    },
+
+    decrementThreshold: function () {
+        console.log("-")
+    },
+
+    incrementThreshold: function () {
+        console.log("+")
+    },
+
+    async save() {
+        try {
+            await settingService.put(window.model.vieScolaire.structure.id, vm.setting);
+            toasts.confirm('presences.alert.manage.event.method.save.confirm');
+        } catch (e) {
+            toasts.confirm('presences.alert.manage.event.method.save.error');
+            throw e;
+        }
+    }
+};
+
+
 export const presencesAlertManage = {
     title: 'presences.alert.title',
     public: false,
@@ -5,18 +65,12 @@ export const presencesAlertManage = {
     controller: {
         init: async function () {
             console.log('alert');
-            // this.vm = vm;
+            this.vm = vm;
             this.setHandler();
             presencesAlertManage.that = this;
-            // vm.safeApply = safeApply;
         },
-
-        removeEvent: function () {
-            console.log("-")
-        },
-
-        addEvent: function () {
-            console.log("+")
+        setHandler: function () {
+            this.$watch(() => window.model.vieScolaire.structure, vm.getThreshold);
         }
     }
 };
