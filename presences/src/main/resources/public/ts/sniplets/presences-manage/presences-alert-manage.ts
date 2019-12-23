1fc1080d67aa778c3962a288420c609a17adf46b
CaroleHermouet <carole.hermouet@cgi.com>
2019-12-23 18:00:56 +0100

    feature(presences): MA-423 Manage Alert (Increment/Decrement + add ng-repeat)

--- presences/src/main/resources/public/ts/sniplets/presences-manage/presences-alert-manage.ts
+++ presences/src/main/resources/public/ts/sniplets/presences-manage/presences-alert-manage.ts
@@ -1,23 +1,31 @@
 import {Setting, settingService} from '../../services/SettingsService';
-import {toasts} from "entcore";
+import {idiom as lang, toasts} from "entcore";
 
 declare const window;
+
+interface InputObject {
+    label: String,
+    threshold: number,
+}
 
 interface ViewModel {
     setting: Setting;
 
+    inputThreshold: Array<InputObject>;
+
     // interaction
     getThreshold(): void;
 
-    incrementThreshold(): void;
+    incrementThreshold(label): void;
 
-    decrementThreshold(): void;
+    decrementThreshold(label): void;
 
     save(): void;
 }
 
 const vm: ViewModel = {
     setting: {},
+    inputThreshold: [],
 
     async getThreshold() {
         try {
@@ -31,6 +39,25 @@ const vm: ViewModel = {
 
             vm.setting = {...defaultSettings, ...structureSettings};
 
+            vm.inputThreshold = [
+                {
+                    label: lang.translate("presences.navigation.absence"),
+                    threshold: vm.setting.alert_absence_threshold
+                },
+                {
+                    label: lang.translate("presences.absence.retards"),
+                    threshold: vm.setting.alert_lateness_threshold
+                },
+                {
+                    label: lang.translate("presences.incidents"),
+                    threshold: vm.setting.alert_incident_threshold
+                },
+                {
+                    label: lang.translate("presences.navigation.notebook"),
+                    threshold: vm.setting.alert_forgotten_notebook_threshold
+                },
+            ];
+
             presencesAlertManage.that.$apply();
         } catch (e) {
             toasts.warning('error');
@@ -38,12 +65,74 @@ const vm: ViewModel = {
         }
     },
 
-    decrementThreshold: function () {
-        console.log("-")
+    decrementThreshold: async function (label) {
+        if (label == lang.translate("presences.navigation.absence")) {
+            if(vm.setting.alert_absence_threshold > 0) {
+                vm.setting.alert_absence_threshold--;
+            }
+            vm.save();
+        }
+        if (label == lang.translate("presences.absence.retards")) {
+            if(vm.setting.alert_lateness_threshold > 0) {
+                vm.setting.alert_lateness_threshold--;
+            }
+            vm.save();
+        }
+        if (label == lang.translate("presences.incidents")) {
+            if(vm.setting.alert_incident_threshold > 0) {
+                vm.setting.alert_incident_threshold--;
+            }
+            vm.save();
+        }
+        if (label == lang.translate("presences.register.event_type.forgotten.notebooks")) {
+            if(vm.setting.alert_forgotten_notebook_threshold > 0) {
+                vm.setting.alert_forgotten_notebook_threshold--;
+            }
+            vm.save();
+        }
     },
 
-    incrementThreshold: function () {
-        console.log("+")
+    incrementThreshold: async function (label) {
+        if(label == lang.translate("presences.navigation.absence")) {
+            if(!vm.setting.alert_absence_threshold) {
+                vm.setting.alert_absence_threshold = 1;
+            }
+            else {
+                vm.setting.alert_absence_threshold ++;
+            }
+            vm.save();
+            await vm.getThreshold();
+        }
+        if(label == lang.translate("presences.absence.retards")) {
+            if(!vm.setting.alert_lateness_threshold) {
+                vm.setting.alert_lateness_threshold = 1;
+            }
+            else {
+                vm.setting.alert_lateness_threshold ++;
+            }
+            vm.save();
+            await vm.getThreshold();
+        }
+        if(label == lang.translate("presences.incidents")) {
+            if(!vm.setting.alert_incident_threshold) {
+                vm.setting.alert_incident_threshold = 1;
+            }
+            else {
+                vm.setting.alert_incident_threshold ++;
+            }
+            vm.save();
+            await vm.getThreshold();
+        }
+        if(label == lang.translate("presences.register.event_type.forgotten.notebooks")) {
+            if(!vm.setting.alert_forgotten_notebook_threshold) {
+                vm.setting.alert_forgotten_notebook_threshold = 1;
+            }
+            else {
+                vm.setting.alert_forgotten_notebook_threshold ++;
+            }
+            vm.save();
+            await vm.getThreshold();
+        }
     },
 
     async save() {
