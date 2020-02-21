dd3a8964d109c08a4da5b5eae9c239fbb046e82b
Simon LEDUNOIS <simon.ledunois@gmail.com>
2020-02-21 17:35:18 +0100

    fix(massmailing) remove request.pause that freez the service

--- massmailing/src/main/java/fr/openent/massmailing/security/BodyCanAccessMassMailing.java
+++ massmailing/src/main/java/fr/openent/massmailing/security/BodyCanAccessMassMailing.java
@@ -14,9 +14,7 @@ import java.util.List;
 public class BodyCanAccessMassMailing implements ResourcesProvider {
     @Override
     public void authorize(HttpServerRequest request, Binding binding, UserInfos userInfos, Handler<Boolean> handler) {
-        request.pause();
         RequestUtils.bodyToJson(request, body -> {
-            request.resume();
             String structureId = body.getString("structure");
             List<String> structures = userInfos.getStructures();
             handler.handle(structures.contains(structureId) && WorkflowHelper.hasRight(userInfos, WorkflowActions.VIEW.toString()));
