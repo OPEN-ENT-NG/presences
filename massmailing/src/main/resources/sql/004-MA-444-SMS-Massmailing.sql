28cc54d6a6ee65c0cbf5d682815be95f921338fb
Simon LEDUNOIS <simon.ledunois@gmail.com>
2020-02-25 16:22:23 +0100

    fix(massmailing) ALTER type constraint on massmailing template table. Add SMS type

--- massmailing/src/main/resources/sql/004-MA-444-SMS-Massmailing.sql
+++ massmailing/src/main/resources/sql/004-MA-444-SMS-Massmailing.sql
@@ -3,3 +3,9 @@ DROP CONSTRAINT type_check;
 
 ALTER TABLE massmailing.mailing
 ADD CONSTRAINT type_check CHECK (type IN ('MAIL', 'SMS'));
+
+ALTER TABLE massmailing.template
+DROP CONSTRAINT type_check;
+
+ALTER TABLE massmailing.template
+ADD CONSTRAINT type_check CHECK (type IN ('MAIL', 'SMS'));
