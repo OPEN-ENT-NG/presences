1facfeaa3476d7620ff5203c6e4042ad530a1cdd
quentin.picard <quentin.picard.18@gmail.com>
2020-10-29 10:57:40 +0100

    feat(presences): MA-681 Edit Category (punishments) as "modèle" (sql / i18n)

---
+++ incidents/src/main/resources/sql/019-MA-497-update-punishment-category-names.sql
@@ -0,0 +1,4 @@
+UPDATE incidents.punishment_category SET label = 'Modèle 1' WHERE label = 'Devoir supplémentaire';
+UPDATE incidents.punishment_category SET label = 'Modèle 2' WHERE label = 'Retenue';
+UPDATE incidents.punishment_category SET label = 'Modèle 3' WHERE label = 'Blâme';
+UPDATE incidents.punishment_category SET label = 'Modèle 4' WHERE label = 'Exclusion';
