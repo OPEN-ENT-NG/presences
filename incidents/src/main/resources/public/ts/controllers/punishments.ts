6798397ba4cca19d91053d4c4255bc0891756e15
flavien.martineau <flavien.martineau@cgi.com>
2021-02-10 10:33:12 +0100

    fix(incidents): MA-754 Display creation date for punishment with null type_id

--- incidents/src/main/resources/public/ts/controllers/punishments.ts
+++ incidents/src/main/resources/public/ts/controllers/punishments.ts
@@ -262,50 +262,50 @@ export const punishmentController = ng.controller('PunishmentController',
             };
 
             vm.getPunishmentDate = (punishment: IPunishment): string => {
-                let createdDate: string = DateUtils.format(punishment.created_at, DateUtils.FORMAT["DAY-MONTH-YEAR"]);
-                switch(punishment.type.punishment_category_id) {
-                    case 1: //DUTY
-                        let dutyDate: string = createdDate;
-                        if ((<IPDutyField>punishment.fields).delay_at) {
-                            dutyDate = DateUtils.format((<IPDutyField>punishment.fields).delay_at, DateUtils.FORMAT["DAY-MONTH-YEAR"]);
-                        }
-                        return lang.translate("incidents.punishments.date.for.the") + dutyDate;
-                    case 2: //DETENTION
-                        let startDetentionDate: string = createdDate;
-                        if ((<IPDetentionField>punishment.fields).start_at) {
-                            startDetentionDate = DateUtils.format((<IPDetentionField>punishment.fields).start_at, DateUtils.FORMAT["DAY-MONTH-YEAR"]);
-                        }
-                        return lang.translate("incidents.punishments.date.for.the") + startDetentionDate;
-                    case 3: //BLAME
-                        return lang.translate("incidents.punishments.date.created.on") + createdDate;
-                    case 4: // EXCLUSION
-                        if ((<IPDetentionField>punishment.fields).start_at && (<IPDetentionField>punishment.fields).end_at) {
-                            let startExcludeDate: string = DateUtils.format((<IPExcludeField>punishment.fields).start_at, DateUtils.FORMAT["DAY-MONTH-YEAR"]);
-                            let endExcludeDate: string = DateUtils.format((<IPExcludeField>punishment.fields).end_at, DateUtils.FORMAT["DAY-MONTH-YEAR"]);
-                            if (startExcludeDate && endExcludeDate) {
-                                return lang.translate("incidents.punishments.date.from") + startExcludeDate + lang.translate("incidents.punishments.date.to") + endExcludeDate;
-                            } else {
-                                return " ";
+                let createdDate: string = DateUtils.format(punishment.created_at, DateUtils.FORMAT['DAY-MONTH-YEAR']);
+
+                if (punishment.type) {
+                    switch (punishment.type.punishment_category_id) {
+                        case 1: // DUTY
+                            let dutyDate: string = createdDate;
+                            if ((<IPDutyField> punishment.fields).delay_at) {
+                                dutyDate = DateUtils.format((<IPDutyField> punishment.fields).delay_at, DateUtils.FORMAT['DAY-MONTH-YEAR']);
                             }
-                        }
-                    default:
-                        return createdDate;
+                            return lang.translate('incidents.punishments.date.for.the') + dutyDate;
+                        case 2: // DETENTION
+                            let startDetentionDate: string = createdDate;
+                            if ((<IPDetentionField> punishment.fields).start_at) {
+                                startDetentionDate = DateUtils.format((<IPDetentionField> punishment.fields).start_at, DateUtils.FORMAT['DAY-MONTH-YEAR']);
+                            }
+                            return lang.translate('incidents.punishments.date.for.the') + startDetentionDate;
+                        case 3: // BLAME
+                            return lang.translate('incidents.punishments.date.created.on') + createdDate;
+                        case 4: // EXCLUSION
+                            if ((<IPDetentionField> punishment.fields).start_at && (<IPDetentionField> punishment.fields).end_at) {
+                                let startExcludeDate: string = DateUtils.format((<IPExcludeField> punishment.fields).start_at, DateUtils.FORMAT['DAY-MONTH-YEAR']);
+                                let endExcludeDate: string = DateUtils.format((<IPExcludeField> punishment.fields).end_at, DateUtils.FORMAT['DAY-MONTH-YEAR']);
+                                if (startExcludeDate && endExcludeDate) {
+                                    return lang.translate('incidents.punishments.date.from') + startExcludeDate + lang.translate('incidents.punishments.date.to') + endExcludeDate;
+                                }
+                            }
+                    }
                 }
+                return lang.translate('incidents.punishments.date.created.on') + createdDate;
             };
 
             vm.getPunishmentTime = (punishment: IPunishment): string => {
-                if (punishment.type.punishment_category_id == 2) { //DETENTION
-                    if ((<IPDetentionField>punishment.fields).start_at && (<IPDetentionField>punishment.fields).end_at) {
-                        let startDetentionDate: string = DateUtils.format((<IPDetentionField>punishment.fields).start_at, DateUtils.FORMAT["HOUR-MIN"]);
-                        let endDetentionDate: string = DateUtils.format((<IPDetentionField>punishment.fields).end_at, DateUtils.FORMAT["HOUR-MIN"]);
+                if (punishment.type && punishment.type.punishment_category_id === 2) { // DETENTION
+                    if ((<IPDetentionField> punishment.fields).start_at && (<IPDetentionField> punishment.fields).end_at) {
+                        let startDetentionDate: string = DateUtils.format((<IPDetentionField> punishment.fields).start_at, DateUtils.FORMAT['HOUR-MIN']);
+                        let endDetentionDate: string = DateUtils.format((<IPDetentionField> punishment.fields).end_at, DateUtils.FORMAT['HOUR-MIN']);
                         if (startDetentionDate && endDetentionDate) {
-                            return startDetentionDate + " - " + endDetentionDate;
+                            return startDetentionDate + ' - ' + endDetentionDate;
                         } else {
-                            return " ";
+                            return ' ';
                         }
                     }
                 }
-                return "";
+                return '';
             };
 
             vm.updateFilter = async (): Promise<void> => {
