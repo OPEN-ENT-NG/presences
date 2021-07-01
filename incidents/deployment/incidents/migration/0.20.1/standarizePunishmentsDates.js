628eb0bf70053f6aa81dcbbed3b248d72e135efd
jinhda douang <s>
2021-07-01 12:54:31 +0200

    fix(incidents): fix mongoDb synthaxe for punishment date format

--- incidents/deployment/incidents/migration/0.20.1/standarizePunishmentsDates.js
+++ incidents/deployment/incidents/migration/0.20.1/standarizePunishmentsDates.js
@@ -1,6 +1,4 @@
-db.getCollection("presences.punishments")
-    .find({created_at: /(.*\/.*)/})
-    .forEach((punishment) => {
-        punishment.created_at = punishment.created_at.replace(/\//g, "-");
-        db.getCollection("presences.punishments").save(punishment);
-    });
+db.getCollection("presences.punishments").find({created_at: /(.*\/.*)/}).forEach(function (punishment) {
+    punishment.created_at = punishment.created_at.replace(/\//g, "-");
+    db.getCollection("presences.punishments").save(punishment);
+});
