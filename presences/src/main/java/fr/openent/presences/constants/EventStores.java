3321f6e3d91435b8226901e6a6d981ccdc2e1931
jinhda douang <s>
2021-11-02 21:16:49 +0100

    feat(eventStore): #MA-858 add eventStore each register that are validated

---
+++ presences/src/main/java/fr/openent/presences/constants/EventStores.java
@@ -0,0 +1,14 @@
+package fr.openent.presences.constants;
+
+public class EventStores {
+
+    private EventStores() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    // ACCESS MODULE VIEW STAT
+    public static final String ACCESS = "ACCESS";
+
+    // CREATE EVENT TO STORE REGISTER VALIDATION
+    public static final String VALIDATE_REGISTER = "VALIDATE_REGISTER";
+}
