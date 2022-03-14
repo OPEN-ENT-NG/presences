4890e13f204ce84f2f558f5328d709a384cab423
Flavien MARTINEAU <45894263+flmartineau@users.noreply.github.com>
2022-03-14 17:45:31 +0100

    feat(statistics): #MA-865 add view.restricted right

--- statistics-presences/src/main/java/fr/openent/statistics_presences/controller/security/UserInStructure.java
+++ statistics-presences/src/main/java/fr/openent/statistics_presences/controller/security/UserInStructure.java
@@ -1,6 +1,7 @@
 package fr.openent.statistics_presences.controller.security;
 
 import fr.openent.presences.common.helper.WorkflowHelper;
+import fr.openent.presences.core.constants.*;
 import fr.openent.statistics_presences.enums.WorkflowActions;
 import fr.wseduc.webutils.http.Binding;
 import io.vertx.core.Handler;
@@ -11,8 +12,9 @@ import org.entcore.common.user.UserInfos;
 public class UserInStructure implements ResourcesProvider {
     @Override
     public void authorize(HttpServerRequest request, Binding binding, UserInfos user, Handler<Boolean> handler) {
-        String structure = request.getParam("structure");
+        String structure = request.getParam(Field.STRUCTURE);
         handler.handle(user.getStructures().contains(structure) &&
-                WorkflowHelper.hasRight(user, WorkflowActions.STATISTICS_PRESENCES_VIEW.toString()));
+                (WorkflowHelper.hasRight(user, WorkflowActions.STATISTICS_PRESENCES_VIEW.toString())
+                || WorkflowHelper.hasRight(user, WorkflowActions.STATISTICS_PRESENCES_VIEW_RESTRICTED.toString())));
     }
 }
