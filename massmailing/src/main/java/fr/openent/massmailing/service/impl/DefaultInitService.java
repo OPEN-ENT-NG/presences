9db50a20ad1b988049547d5fd1c12b06aaa6402e
BastienBenard <95362455+BastienBenard@users.noreply.github.com>
2022-06-15 17:03:21 +0200

    fix(massmailing): #MA-957 fix init massmiling (#132)
    Co-authored-by: Benard Bastien <bastien.benard@cgi.com>

--- massmailing/src/main/java/fr/openent/massmailing/service/impl/DefaultInitService.java
+++ massmailing/src/main/java/fr/openent/massmailing/service/impl/DefaultInitService.java
@@ -13,30 +13,34 @@ import org.entcore.common.http.request.JsonHttpServerRequest;
 public class DefaultInitService implements InitService {
     @Override
     public void getTemplateStatement(JsonHttpServerRequest request, String structure, String owner, Handler<Either<String, JsonObject>> handler) {
-        Integer occurrences = 2;
+        final int occurrences = 2;
         JsonArray params = new JsonArray();
-        String query = "INSERT INTO " + Massmailing.dbSchema + ".template(structure_id, name, content, type, owner) VALUES ";
+        String query = "INSERT INTO " + Massmailing.dbSchema + ".template(structure_id, name, content, type, owner, category) VALUES ";
 
         for (int i = 0; i < occurrences; i++) {
             String mailName = I18n.getInstance().translate("massmailing.init.template.mail." + i + ".name", Renders.getHost(request), I18n.acceptLanguage(request));
             String mailContent = I18n.getInstance().translate("massmailing.init.template.mail." + i + ".content", Renders.getHost(request), I18n.acceptLanguage(request));
-            query += "(?, ?, ?, ?, ?),";
+            String mailCategory = I18n.getInstance().translate("massmailing.init.template.mail." + i + ".category", Renders.getHost(request), I18n.acceptLanguage(request));
+            query += "(?, ?, ?, ?, ?, ?),";
             params.add(structure)
                     .add(mailName)
                     .add(mailContent)
                     .add("MAIL")
-                    .add(owner);
+                    .add(owner)
+                    .add(mailCategory);
         }
 
         for (int i = 0; i < occurrences; i++) {
             String smsName = I18n.getInstance().translate("massmailing.init.template.sms." + i + ".name", Renders.getHost(request), I18n.acceptLanguage(request));
             String smsContent = I18n.getInstance().translate("massmailing.init.template.sms." + i + ".content", Renders.getHost(request), I18n.acceptLanguage(request));
-            query += "(?, ?, ?, ?, ?),";
+            String smsCategory = I18n.getInstance().translate("massmailing.init.template.sms." + i + ".category", Renders.getHost(request), I18n.acceptLanguage(request));
+            query += "(?, ?, ?, ?, ?, ?),";
             params.add(structure)
                     .add(smsName)
                     .add(smsContent)
                     .add("SMS")
-                    .add(owner);
+                    .add(owner)
+                    .add(smsCategory);
         }
 
         query = query.substring(0, query.length() - 1) + ";";
