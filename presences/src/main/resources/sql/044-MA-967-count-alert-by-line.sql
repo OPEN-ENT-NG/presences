8c40cbf138dd034b8c2071c3540c33f2b3eba2c2
Benard Bastien <bastien.benard@cgi.com>
2022-06-23 10:50:58 +0200

    feat(alert): #MA-967 add of the count of alerts per line

---
+++ presences/src/main/resources/sql/044-MA-967-count-alert-by-line.sql
@@ -0,0 +1,17 @@
+-- Delete old data due to the modification of the table
+TRUNCATE TABLE presences.alerts;
+TRUNCATE TABLE presences.alert_history;
+
+-- Override unique CONSTRAINT
+ALTER TABLE presences.alerts ADD COLUMN event_id bigint NOT NULL,
+                             DROP CONSTRAINT uniq_alert,
+                             ADD CONSTRAINT uniq_alert UNIQUE (event_id, structure_id, student_id, type),
+                             -- Delete column no longer used
+                             DROP COLUMN exceed_date,
+                             DROP COLUMN modified,
+                             -- Column used in trigger
+                             ADD COLUMN delete_at timestamp without time zone;
+
+ALTER TABLE presences.alert_history DROP COLUMN exceed_date,
+                                    DROP COLUMN modified,
+                                    ADD COLUMN delete_at timestamp without time zone NOT NULL;
