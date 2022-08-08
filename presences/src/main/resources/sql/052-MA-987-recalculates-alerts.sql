0ad416e01566f0d4db37177ef3b01307a0a61c38
Benard Bastien <bastien.benard@cgi.com>
2022-08-08 16:03:57 +0200

    fix(alert) #MA-987 recalculates only year alert

--- presences/src/main/resources/sql/052-MA-987-recalculates-alerts.sql
+++ presences/src/main/resources/sql/052-MA-987-recalculates-alerts.sql
@@ -1,40 +1,52 @@
 do $$
     declare
-        structureId varchar;
-        eventItems presences.event;
+        eventRegisterItem record;
+        registerItem presences.register;
+        eventItem presences.event;
         forgottenNotebook presences.forgotten_notebook;
         incident incidents.incident;
         protagonist incidents.protagonist;
         existingAbsenceWithAlert bigint;
     begin
         TRUNCATE presences.alerts, presences.alert_history;
-        FOR eventItems IN SELECT * FROM presences.event LEFT JOIN presences.alerts a on event.id = a.event_id WHERE a IS NULL LOOP
-                SELECT structure_id FROM presences.register WHERE id = eventItems.register_id INTO structureId;
-                if (eventItems.type_id = 1) THEN
+        FOR eventRegisterItem IN SELECT e as event, r as register
+                                FROM presences.event e
+                                INNER JOIN presences.register r on e.register_id = r.id
+                                LEFT JOIN viesco.setting_period s on r.structure_id = s.id_structure
+                                LEFT JOIN presences.alerts a on e.id = a.event_id
+                                WHERE a IS NULL AND s.code = 'YEAR' AND r.start_date >= s.start_date LOOP
+                registerItem = (eventRegisterItem.register);
+                eventItem = (eventRegisterItem.event);
+                IF (eventItem.type_id = 1) THEN
                     existingAbsenceWithAlert = NULL;
-                    SELECT * FROM presences.get_id_absence_event_siblings(eventItems, structureId, true) INTO existingAbsenceWithAlert;
-                    IF existingAbsenceWithAlert IS NULL AND NOT presences.absence_exclude_alert(eventItems, structureId) THEN -- If we have no exclude condition and not siblings
-                        INSERT INTO presences.alerts(student_id, structure_id, type, event_id, created) VALUES (eventItems.student_id , structureId, 'ABSENCE', eventItems.id, eventItems.created);
+                    SELECT * FROM presences.get_id_absence_event_siblings(eventItem, registerItem.structure_id, true) INTO existingAbsenceWithAlert;
+                    IF existingAbsenceWithAlert IS NULL AND NOT presences.absence_exclude_alert(eventItem, registerItem.structure_id) THEN -- If we have no exclude condition and not siblings
+                        INSERT INTO presences.alerts(student_id, structure_id, type, event_id, date) VALUES (eventItem.student_id , registerItem.structure_id, 'ABSENCE', eventItem.id, registerItem.start_date);
                     END IF;
                 ELSE
-                    IF NOT presences.lateness_exclude_alert(eventItems, structureId) THEN -- If we have no exclude condition
+                    IF NOT presences.lateness_exclude_alert(eventItem, registerItem.structure_id) THEN -- If we have no exclude condition
                     -- Create alert
-                        INSERT INTO presences.alerts(student_id, structure_id, type, event_id, created) VALUES (eventItems.student_id , structureId, 'LATENESS', eventItems.id, eventItems.created);
+                        INSERT INTO presences.alerts(student_id, structure_id, type, event_id, date) VALUES (eventItem.student_id , registerItem.structure_id, 'LATENESS', eventItem.id, registerItem.start_date);
                     END IF;
                 end if;
             end loop;
 
-        FOR forgottenNotebook IN SELECT * FROM presences.forgotten_notebook as f LEFT JOIN presences.alerts a on f.id = a.event_id WHERE a IS NULL LOOP
-                IF presences.notebook_exclude_alert(structureId) IS FALSE THEN -- If we have no exclude condition
+        FOR forgottenNotebook IN SELECT * FROM presences.forgotten_notebook as f
+                                        LEFT JOIN viesco.setting_period s on f.structure_id = s.id_structure
+                                        LEFT JOIN presences.alerts a on f.id = a.event_id
+                                        WHERE a IS NULL AND s.code = 'YEAR' AND f.date >= s.start_date LOOP
+                IF presences.notebook_exclude_alert(forgottenNotebook.structure_id) IS FALSE THEN -- If we have no exclude condition
                 -- Create alert
-                    INSERT INTO presences.alerts(student_id, structure_id, type, event_id, created) VALUES (forgottenNotebook.student_id , forgottenNotebook.structure_id, 'FORGOTTEN_NOTEBOOK', forgottenNotebook.id, forgottenNotebook.created);
+                    INSERT INTO presences.alerts(student_id, structure_id, type, event_id, date) VALUES (forgottenNotebook.student_id , forgottenNotebook.structure_id, 'FORGOTTEN_NOTEBOOK', forgottenNotebook.id, forgottenNotebook.date);
                 END IF;
             end loop;
 
-        FOR incident IN SELECT * FROM incidents.incident AS i WHERE incidents.incident_exclude_alert(i) IS FALSE LOOP
+        FOR incident IN SELECT * FROM incidents.incident AS i
+                        LEFT JOIN viesco.setting_period s on i.structure_id = s.id_structure
+                        WHERE incidents.incident_exclude_alert(i) IS FALSE AND s.code = 'YEAR' AND i.date >= s.start_date LOOP
                 FOR protagonist IN SELECT * FROM incidents.protagonist as p WHERE p.incident_id = incident.id AND NOT exists(SELECT * FROM presences.alerts as a WHERE a.type = 'INCIDENT' AND a.event_id = incident.id)
                     LOOP
-                        INSERT INTO presences.alerts(student_id, structure_id, type, event_id, created) VALUES (protagonist.user_id, incident.structure_id, 'INCIDENT', incident.id, incident.created);
+                        INSERT INTO presences.alerts(student_id, structure_id, type, event_id, date) VALUES (protagonist.user_id, incident.structure_id, 'INCIDENT', incident.id, incident.date);
                     end loop;
             end loop;
     end$$;
