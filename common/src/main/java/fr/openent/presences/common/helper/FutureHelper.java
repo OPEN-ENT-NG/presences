d5710b6c4cfa77b15ef516b394816491389c64c7
jinhda douang <s>
2022-08-08 17:40:20 +0200

    refactor(future-helper): add future helper method dynamically

--- common/src/main/java/fr/openent/presences/common/helper/FutureHelper.java
+++ common/src/main/java/fr/openent/presences/common/helper/FutureHelper.java
@@ -1,5 +1,4 @@
 package fr.openent.presences.common.helper;
-
 import fr.wseduc.webutils.Either;
 import fr.openent.presences.core.constants.Field;
 import io.vertx.core.*;
@@ -9,16 +8,11 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-
 import java.util.List;
-
 public class FutureHelper {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(FutureHelper.class);
-
     private FutureHelper() {
     }
-
     public static Handler<Either<String, JsonArray>> handlerJsonArray(Future<JsonArray> future) {
         return event -> {
             if (event.isRight()) {
@@ -29,7 +23,10 @@ public class FutureHelper {
             }
         };
     }
-
+    /**
+     * @deprecated  Replaced by {@link #handlerEitherPromise(Promise)}
+     */
+    @Deprecated
     public static Handler<Either<String, JsonArray>> handlerJsonArray(Promise<JsonArray> promise) {
         return event -> {
             if (event.isRight()) {
@@ -42,7 +39,10 @@ public class FutureHelper {
             }
         };
     }
-
+    /**
+     * @deprecated  Replaced by {@link #handlerEitherPromise(Promise)}
+     */
+    @Deprecated
     public static Handler<Either<String, JsonObject>> handlerJsonObject(Promise<JsonObject> promise) {
         return event -> {
             if (event.isRight()) {
@@ -55,7 +55,18 @@ public class FutureHelper {
             }
         };
     }
-
+    public static <L, R> Handler<Either<L, R>> handlerEitherPromise(Promise<R> promise) {
+        return event -> {
+            if (event.isRight()) {
+                promise.complete(event.right().getValue());
+            } else {
+                String message = String.format("[PresencesCommon@%s::handlerFuture]: %s",
+                        FutureHelper.class.getSimpleName(), event.left().getValue());
+                LOGGER.error(message);
+                promise.fail(event.left().getValue().toString());
+            }
+        };
+    }
     public static Handler<Either<String, JsonArray>> handlerJsonArray(Handler<AsyncResult<JsonArray>> handler) {
         return event -> {
             if (event.isRight()) {
@@ -66,7 +77,6 @@ public class FutureHelper {
             }
         };
     }
-
     public static Handler<AsyncResult<JsonArray>> handlerAsyncJsonArray(Future<JsonArray> future) {
         return event -> {
             if (event.succeeded()) {
@@ -77,7 +87,6 @@ public class FutureHelper {
             }
         };
     }
-
     public static Handler<Either<String, JsonObject>> handlerJsonObject(Future<JsonObject> future) {
         return event -> {
             if (event.isRight()) {
@@ -88,7 +97,6 @@ public class FutureHelper {
             }
         };
     }
-
     public static Handler<Either<String, JsonObject>> handlerJsonObject(Handler<AsyncResult<JsonObject>> handler) {
         return event -> {
             if (event.isRight()) {
@@ -99,19 +107,16 @@ public class FutureHelper {
             }
         };
     }
-
     public static void busArrayHandler(Future<JsonArray> future, Message<JsonObject> message) {
         future
                 .onSuccess(result -> message.reply((new JsonObject()).put(Field.STATUS, Field.OK).put(Field.RESULT, result)))
                 .onFailure(error -> message.reply((new JsonObject()).put(Field.STATUS, Field.ERROR).put(Field.MESSAGE, error.getMessage())));
     }
-
     public static void busObjectHandler(Future<JsonObject> future, Message<JsonObject> message) {
         future
                 .onSuccess(result -> message.reply((new JsonObject()).put(Field.STATUS, Field.OK).put(Field.RESULT, result)))
                 .onFailure(error -> message.reply((new JsonObject()).put(Field.STATUS, Field.ERROR).put(Field.MESSAGE, error.getMessage())));
     }
-
     public static void handleObjectResult(JsonObject messageBody, Promise<JsonObject> promise) {
         if (Field.OK.equals(messageBody.getString(Field.STATUS)))
             promise.complete(messageBody.getJsonObject(Field.RESULT, new JsonObject()));
@@ -120,11 +125,9 @@ public class FutureHelper {
             promise.fail(messageBody.getString(Field.MESSAGE));
         }
     }
-
     public static <T> CompositeFuture all(List<Future<T>> futures) {
         return CompositeFutureImpl.all(futures.toArray(new Future[futures.size()]));
     }
-
     public static <T> CompositeFuture join(List<Future<T>> futures) {
         return CompositeFutureImpl.join(futures.toArray(new Future[futures.size()]));
     }
