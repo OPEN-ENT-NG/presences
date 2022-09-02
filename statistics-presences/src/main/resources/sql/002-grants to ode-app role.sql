a277f46ca78a6f7a09e6674f39f4e3eb588c6e4b
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2022-09-02 15:57:01 +0200

    feat(postgres): #WB-555, grant privileges to apps role

---
+++ statistics-presences/src/main/resources/sql/002-grants to ode-app role.sql
@@ -0,0 +1,4 @@
+GRANT USAGE ON SCHEMA presences_statistics TO "apps";
+GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON ALL TABLES IN SCHEMA presences_statistics TO "apps";
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA presences_statistics TO "apps";
+GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA presences_statistics TO "apps";
