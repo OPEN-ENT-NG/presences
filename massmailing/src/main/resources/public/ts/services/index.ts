9edc4961b7a5bf36700c468b6e3e860ee96515ff
BastienBenard <95362455+BastienBenard@users.noreply.github.com>
2022-09-16 09:35:14 +0200

    feat(search): #191 add grouping to search (common) (#192)
    Co-authored-by: Benard Bastien <bastien.benard@cgi.com>

--- massmailing/src/main/resources/public/ts/services/index.ts
+++ massmailing/src/main/resources/public/ts/services/index.ts
@@ -3,6 +3,7 @@ export * from './MassmailingService';
 export * from '@presences/services/ReasonService'
 export * from '@common/services/SearchService';
 export * from '@common/services/GroupService';
+export * from '@common/services/grouping.service';
 export * from '@incidents/services/PunishmentTypeService';
 export * from '@incidents/services/PunishmentService';
 export * from './MailingService';
