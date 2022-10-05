52a2b0f3fe2cde18d3b04a8ba6954287ab1d5a2f
BastienBenard <95362455+BastienBenard@users.noreply.github.com>
2022-10-05 18:32:33 +0200

    fix(workflow): #MA-1008 fix workflow right (#220)
    Co-authored-by: Benard Bastien <bastien.benard@cgi.com>

--- statistics-presences/src/main/java/fr/openent/statistics_presences/enums/WorkflowActionsCouple.java
+++ statistics-presences/src/main/java/fr/openent/statistics_presences/enums/WorkflowActionsCouple.java
@@ -15,11 +15,11 @@ public enum WorkflowActionsCouple implements IWorkflowActionsCouple {
 
     @Override
     public String getUnrestrictedAction() {
-        return unrestrictedAction.name();
+        return unrestrictedAction.toString();
     }
 
     @Override
     public String getRestrictedAction() {
-        return restrictedAction.name();
+        return restrictedAction.toString();
     }
 }
