9c5818070cc3384166acab83d40705e450261d3e
Benard Bastien <bastien.benard@cgi.com>
2022-11-23 09:12:26 +0100

    feat(stats): #MA-1032 delete statistics when a user is deleted

--- statistics-presences/src/main/java/fr/openent/statistics_presences/service/CommonServiceFactory.java
+++ statistics-presences/src/main/java/fr/openent/statistics_presences/service/CommonServiceFactory.java
@@ -3,17 +3,23 @@ package fr.openent.statistics_presences.service;
 import fr.openent.presences.common.service.*;
 import fr.openent.presences.common.service.impl.*;
 import fr.openent.statistics_presences.service.impl.DefaultStatisticsPresencesService;
+import fr.openent.statistics_presences.service.impl.DefaultStatisticsService;
 import fr.openent.statistics_presences.service.impl.DefaultStatisticsWeeklyAudiencesService;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonArray;
+
+import java.util.Map;
 
 public class CommonServiceFactory {
     private final Vertx vertx;
     private final StatisticsPresencesService statisticsPresencesService;
+    private final StatisticsService statisticsService;
 
     public CommonServiceFactory(Vertx vertx) {
         this.vertx = vertx;
         statisticsPresencesService = new DefaultStatisticsPresencesService(this);
+        statisticsService = new DefaultStatisticsService("");
     }
 
     public UserService userService() {
@@ -22,6 +28,10 @@ public class CommonServiceFactory {
 
     public GroupService groupService() { return new DefaultGroupService(this.vertx.eventBus()); }
 
+    /**
+     * @deprecated Replaced by {@link #getStatisticsPresencesService()}
+     */
+    @Deprecated
     public StatisticsPresencesService statisticsPresencesService() {
         return new DefaultStatisticsPresencesService(this);
     }
@@ -40,5 +50,9 @@ public class CommonServiceFactory {
 
     public StatisticsPresencesService getStatisticsPresencesService() {
         return statisticsPresencesService;
+    }
+
+    public StatisticsService getStatisticsService() {
+        return statisticsService;
     }
 }
