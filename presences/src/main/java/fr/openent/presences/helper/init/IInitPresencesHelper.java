a7996d37661aa914de640868db169eb3f7b60abc
BastienBenard <95362455+BastienBenard@users.noreply.github.com>
2022-11-29 10:39:13 +0100

    fix(init): #MA-997 fix init alert rule (#268)
    Co-authored-by: Benard Bastien <bastien.benard@cgi.com>

--- presences/src/main/java/fr/openent/presences/helper/init/IInitPresencesHelper.java
+++ presences/src/main/java/fr/openent/presences/helper/init/IInitPresencesHelper.java
@@ -6,8 +6,8 @@ import fr.openent.presences.model.*;
 import java.util.List;
 
 public interface IInitPresencesHelper {
-    Init1DPresencesHelper init1DPresencesHelper = new Init1DPresencesHelper();
-    Init2DPresencesHelper init2DPresencesHelper = new Init2DPresencesHelper();
+    DefaultInit1DPresencesHelper defaultInit1DPresencesHelper = new DefaultInit1DPresencesHelper();
+    DefaultInit2DPresencesHelper defaultInit2DPresencesHelper = new DefaultInit2DPresencesHelper();
 
     List<ReasonModel> getReasonsInit();
 
@@ -17,7 +17,12 @@ public interface IInitPresencesHelper {
 
     List<Discipline> getDisciplinesInit();
 
-    static IInitPresencesHelper getInstance(InitTypeEnum initTypeEnum) {
-        return (initTypeEnum == InitTypeEnum.ONE_D) ? init1DPresencesHelper : init2DPresencesHelper;
+    //Must be changed if implementing a different function per platform
+    static IInitPresencesHelper getInstance(InitTypeEnum initTypeEnum, String ignoredPlatform) {
+        return getDefaultInstance(initTypeEnum);
+    }
+
+    static IInitPresencesHelper getDefaultInstance(InitTypeEnum initTypeEnum) {
+        return (initTypeEnum == InitTypeEnum.ONE_D) ? defaultInit1DPresencesHelper : defaultInit2DPresencesHelper;
     }
 }
