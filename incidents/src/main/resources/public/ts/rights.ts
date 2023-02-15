277ceb416a73eacdcca92b163ac9e52437f8505a
Q3 <quentin.picard@cgi.com>
2023-02-15 16:03:56 +0100

    fix(statistics): #MA-1058 fix right checked before getting punishment types .

--- incidents/src/main/resources/public/ts/rights.ts
+++ incidents/src/main/resources/public/ts/rights.ts
@@ -1,7 +1,7 @@
 const incidentsRights = {
     workflow: {
         access: 'fr.openent.incidents.controller.IncidentsController|view',
-        incidentRead: 'fr.openent.incidents.controller.IncidentsController|incidentRead',
+        incidentRead: 'fr.openent.incidents.controller.IncidentsController|getIncidents',
         readPunishment: 'fr.openent.incidents.controller.FakeRight|punishmentsView',
         readSanction: 'fr.openent.incidents.controller.FakeRight|sanctionsView',
         createPunishment: 'fr.openent.incidents.controller.FakeRight|punishmentCreate',
