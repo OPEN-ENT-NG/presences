ece503150132f334b70bed8626afb51477aa313b
florent.mariotti <florent.mariotti@cgi.com>
2023-06-09 17:41:49 +0200

    feat(parents) : #MA-1011 Javadoc

--- presences/src/main/java/fr/openent/presences/service/SettingsService.java
+++ presences/src/main/java/fr/openent/presences/service/SettingsService.java
@@ -6,16 +6,33 @@ import io.vertx.core.*;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
+import java.util.List;
 import java.util.Map;
 
 public interface SettingsService {
+
+    /**
+     * Retrieve presences settings with a handler.
+     *
+     * @param structureId Structure identifier
+     * @param handler handler
+     * @deprecated Replaced by {@link #retrieveSettings(String)}
+     */
     void retrieve(String structureId, Handler<Either<String, JsonObject>> handler);
 
     /**
+     * Retrieve presences settings with a future of JsonObject.
+     *
+     * @param structureId Structure identifier
      * @deprecated Replaced by {@link #retrieveSettings(String)}
      */
     Future<JsonObject> retrieve(String structureId);
 
+    /**
+     * Retrieve presences settings.
+     *
+     * @param structureId Structure identifier
+     */
     Future<Settings> retrieveSettings(String structureId);
 
     /**
