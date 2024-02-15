3be2dd1997026ee40626f70daa215899e189a701
Florent Mariotti <florentcorse@gmail.com>
2024-02-15 09:37:46 +0100

    fix(notification) : #MA-1221 fix notification grammar wrong with feminine words in french. (#366)
    Co-authored-by: florent.mariotti <florent.mariotti@cgi.com>

--- presences/src/main/java/fr/openent/presences/service/impl/DefaultEventService.java
+++ presences/src/main/java/fr/openent/presences/service/impl/DefaultEventService.java
@@ -1548,16 +1548,20 @@ public class DefaultEventService extends DBService implements EventService {
         Promise<Void> promise = Promise.promise();
 
         JsonObject params = new JsonObject().put(Field.RESOURCEURI, "/presences#/dashboard");
-        String notificationTitle, notificationName;
+        String notificationTitle, notificationName, eventMode;
 
         if(event.getString(Field.OLDEVENTTYPE) != null){ //If we have the oldEventType, it means we are in edition mode, else in creation
             notificationTitle = "presences.push.event.edited";
             notificationName = "presences.event-update";
-            params.put(Field.OLDEVENTTYPE, event.getString(Field.OLDEVENTTYPE));
+            eventMode = ".edition";
+            params.put(Field.OLDEVENTTYPE, I18n.getInstance()
+                    .translate(event.getString(Field.OLDEVENTTYPE) + eventMode, Renders.getHost(request), I18n.acceptLanguage(request)));
+
         }
         else {
             notificationTitle = "presences.push.event.created";
             notificationName = "presences.event-creation";
+            eventMode = ".creation";
         }
 
         params.put(Field.PUSHNOTIF, new JsonObject()
@@ -1571,7 +1575,7 @@ public class DefaultEventService extends DBService implements EventService {
                 })
                 .compose(eventType -> {
                     params.put(Field.EVENTTYPE, I18n.getInstance()
-                            .translate(eventType.getLabel() + Field.DOTNOTIFICATION, Renders.getHost(request), I18n.acceptLanguage(request)));
+                            .translate(eventType.getLabel() + Field.DOTNOTIFICATION + eventMode, Renders.getHost(request), I18n.acceptLanguage(request)));
                     return Viescolaire.getInstance().getResponsables(event.getString(Field.STUDENT_ID));
                 })
                 .onSuccess(responsablesIds -> {
