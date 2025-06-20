bb3adf28429ae7367720e08dc09e44b686784b5d
david-cc <david.caudeli@web-education.net>
2025-06-20 16:12:25 +0200

    fix: #COCO-3968, add Holidays zone (Corse, Guadeloupe, Guyane, ...)

--- presences/src/main/resources/public/ts/services/initialization.service.ts
+++ presences/src/main/resources/public/ts/services/initialization.service.ts
@@ -1,7 +1,8 @@
-import {ng} from 'entcore'
+import {ng, idiom} from 'entcore'
 import http, {AxiosResponse} from 'axios';
 import {INIT_TYPE} from "../core/enum/init-type";
-import {InitForm} from "@presences/models/init-form.model";
+import { HOLIDAYS_ZONE, IInitFormZone, InitForm } from '../models/init-form.model';
+
 export interface IInitStatusResponse {
     initialized: boolean;
 }
@@ -20,6 +21,7 @@ export interface IInitService {
     initViesco(structureId: string, initType: INIT_TYPE, initForm: InitForm): Promise<AxiosResponse>;
 
     getTeachersInitializationStatus(structureId: string): Promise<IInitTeachersResponse>;
+    getZones(): IInitFormZone[];
 
 }
 
@@ -47,8 +49,44 @@ export const initService: IInitService = {
     getTeachersInitializationStatus: async (structureId: string): Promise<IInitTeachersResponse> => {
         return http.get(`/viescolaire/structures/${structureId}/initialization/teachers`)
             .then((res: AxiosResponse) => res.data);
-    }
+    },
 
+    getZones: (): IInitFormZone[] => {
+        const zones: IInitFormZone[] = [];
+
+        zones.push({value: HOLIDAYS_ZONE.ZONE_A, label: idiom.translate("presences.init.1d.form.holidays.detected.zone.A")});
+        zones.push({value: HOLIDAYS_ZONE.ZONE_B, label: idiom.translate("presences.init.1d.form.holidays.detected.zone.B")});
+        zones.push({value: HOLIDAYS_ZONE.ZONE_C, label: idiom.translate("presences.init.1d.form.holidays.detected.zone.C")});
+
+        zones.push({value: HOLIDAYS_ZONE.CORSE, label: idiom.translate("presences.init.1d.form.holidays.detected.zone.Corse")});
+        zones.push({value: HOLIDAYS_ZONE.GUADELOUPE, label: idiom.translate("presences.init.1d.form.holidays.detected.zone.Guadeloupe")});
+        zones.push({value: HOLIDAYS_ZONE.GUYANE, label: idiom.translate("presences.init.1d.form.holidays.detected.zone.Guyane")});
+        zones.push({value: HOLIDAYS_ZONE.MARTINIQUE, label: idiom.translate("presences.init.1d.form.holidays.detected.zone.Martinique")});
+        zones.push({value: HOLIDAYS_ZONE.MAYOTTE, label: idiom.translate("presences.init.1d.form.holidays.detected.zone.Mayotte")});
+        // zones.push({value: HOLIDAYS_ZONE.NOUVELLE_CALEDONIE, label: idiom.translate("presences.init.1d.form.holidays.detected.zone.NouvelleCaledonie")});
+        zones.push({value: HOLIDAYS_ZONE.POLYNESIE, label: idiom.translate("presences.init.1d.form.holidays.detected.zone.Polynesie")});
+        zones.push({value: HOLIDAYS_ZONE.REUNION, label: idiom.translate("presences.init.1d.form.holidays.detected.zone.Reunion")});
+        zones.push({value: HOLIDAYS_ZONE.SAINT_PIERRE_ET_MIQUELON, label: idiom.translate("presences.init.1d.form.holidays.detected.zone.SaintPierreEtMiquelon")});
+        // zones.push({value: HOLIDAYS_ZONE.WALLIS_ET_FUTUNA, label: idiom.translate("presences.init.1d.form.holidays.detected.zone.WallisEtFutuna")});
+
+        return zones;
+
+        
+        // const currentYear = new Date().getFullYear();
+        // const schoolYear = `${currentYear}-${currentYear + 1}`;
+        // const dataGouvAPIUrl = `https://data.education.gouv.fr/api/records/1.0/search/?dataset=fr-en-calendrier-scolaire&refine.annee_scolaire=${schoolYear}&rows=1000`;
+
+        // const zonesResponse = await http.get(dataGouvAPIUrl);
+        // const zones = new Set<string>();
+        // zonesResponse?.data?.records.forEach(record => {
+        //     console.log(record);
+        //     if (record.fields.zones) {
+        //         zones.add(record.fields.zones);
+        //     }
+        // });
+        // console.log(Array.from(zones));
+        // return zones;
+    }
 };
 
 export const InitService = ng.service('InitService', (): IInitService => initService);
